52e3bd130bdc4fb00fa94e69585343d6dd2f8e14
outshaker <wind.sixwings@gmail.com>
2023-08-09 20:44:14 +0800

    feat: improve perf
    - cookie.css, header.css 合併到 shared.css
      因為 main.css 內部包含 lazy4 css 框架，檔案容量過大，已經不適合做為樣式合併的檔案了
    - main.css 停用動畫已改善 FCP, LCP
    - 停用 fontawesome，將使用到的圖示轉成 svg 檔案
    - header.js, home.js 內容整併到 main.js，加快頁面載入時間
    - index.html 調整 div.carousel 的內容，toBannerLink() 改回 <a href...>

--- improved/js/main.js
+++ improved/js/main.js
@@ -1,3 +1,245 @@
+(function () {
+  $(document).ready(function () {
+    const productOwl = $(".productCarouselSection .owl-carousel");
+    productOwl.owlCarousel({
+      loop: true,
+      items: 5,
+      nav: false,
+      dots: false,
+      responsive: {
+        768: { items: 4 },
+        1024: { items: 5 },
+      },
+    });
+
+    $(".product-carousel__nav--prev").click(function () {
+      productOwl.trigger("prev.owl.carousel");
+    });
+
+    $(".product-carousel__nav--next").click(function () {
+      productOwl.trigger("next.owl.carousel");
+    });
+
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    const $kvCarousel = $('.kv-carousel.owl-carousel');
+    $kvCarousel.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        dots: isMobile ? false : true,
+        autoplayTimeout: 8000,
+        autoplayHoverPause: true,
+        video: true
+    });
+
+    // NOTE disabled
+    /*
+    const workspaceOwl = $(".workspace-carousel.owl-carousel"); 
+    workspaceOwl.owlCarousel({
+      // loop: true,
+      items: 3,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 2,
+          center: true,
+          margin: 16,
+          autoWidth: true,
+        },
+        768: { items: 2 },
+        1200: { items: 3 },
+      },
+    });
+
+    $(".workspace-carousel__nav--prev").click(function () {
+      workspaceOwl.trigger("prev.owl.carousel");
+      workspaceOwl.trigger("prev.owl.carousel");
+      workspaceOwl.trigger("prev.owl.carousel");
+    });
+
+    $(".workspace-carousel__nav--next").click(function () {
+      workspaceOwl.trigger("next.owl.carousel");
+      workspaceOwl.trigger("next.owl.carousel");
+      workspaceOwl.trigger("next.owl.carousel");
+    });*/
+  });
+})();
+
+Vue.component('search-bar', {
+    template:
+        `
+      <div class="sPanel" :class="{'is:on': searchPanel}" >
+        <div class="sPanel_wrap" style="z-index: 9999" >
+            <header class="sPanel_head">
+                <input ref="search" id="productSearch" type="search" class="input" v-model="keyword" @keyup="keywordSearch" placeholder="` +
+        msg_p_header_search_placeholder +
+        `">
+                <div class="sPanel_btn">
+                    <button class="btn" @click="keywordSearch()">` +
+        msg_p_header_search +
+        `</button>
+                </div>
+            </header>
+            <main v-if="status.searchMainInfo" class="sPanel_body">
+                <div v-if="status.searchEmptyInfo" class="sPanel_noResult">` +
+        msg_p_header_not_any_result +
+        `</div>
+                <ol v-else class="sPanel_result">
+                    <template v-for="(item, key) in searchResult">
+                        <li class="sPanel_item">
+                            <button style="border: none;background: unset;color: #00AEEF;margin: 8px;font-size: 16px;" @click="keywordSearchRecords(item.id,item.url_title,item.sku_no)">
+                            <span class="sPanel_icon">
+                                <i class="icon fas fa-chevron-right"></i>
+                            </span>{{ item.name }} 
+                            <span class= "sPanel_label">{{ item.sku_no }}</span>
+                        </button>
+                        </li>
+                    </template>
+    
+                </ol>
+            </main>
+        </div>
+        <div class="overlay" @click="() => this.$emit('search-panel-switch')"></div>
+    </div>
+    `,
+    props: ['searchPanel'],
+    data() {
+        return {
+            keyword: '',
+            searchResult: {},
+            status: {
+                searchMainInfo: false,
+                searchEmptyInfo: false,
+            },
+        };
+    },
+    methods: {
+        keywordSearch(e) {
+            let self = this;
+
+            if (this.keyword.length === 0) {
+                self.status.searchMainInfo = false;
+                self.status.searchEmptyInfo = false;
+                self.searchResult = {};
+                return;
+            }
+
+            if (this.keyword.length >= 3 || e === undefined || e.keyCode === 13) {
+                if (
+                    this.keyword.toLowerCase().startsWith('ib-') &&
+                    this.keyword.length < 7
+                ) {
+                    return;
+                }
+
+                let postData = {
+                    locale: lang,
+                    keyword: this.keyword,
+                };
+                $.ajax({
+                    type: 'post',
+                    url: apiUrl + 'search/searchProducts',
+                    data: postData,
+                    success: function (data) {
+                        self.searchResult = data.data;
+                        self.status.searchMainInfo = true;
+
+                        if (data.data.length > 0) {
+                            self.status.searchEmptyInfo = false;
+                        } else {
+                            self.status.searchEmptyInfo = true;
+                        }
+                    },
+                    dataType: 'json',
+                });
+            }
+        },
+        keywordSearchRecords(id, title, no) {
+            let urlPrefix = '';
+            if (window.location.hostname == 'localhost') {
+                urlPrefix = '/lanFront/';
+            }
+
+            let postData = {
+                keyWord: $('#productSearch').val(),
+                productId: id
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: apiUrl + 'set/searchKeyword',
+                data: postData,
+                success: function (data) {
+                },
+                dataType: 'json',
+            });
+
+            window.location.assign(urlPrefix + '/' + title + '/' + no);
+        },
+    },
+});
+
+Vue.component('cookie-banner', {
+    template: `
+    <div class="cookie-banner" v-if="!isHidden">
+        <div class="wrapper">
+            <div class="content">
+              ${cookie_banner_content}
+            </div>
+            <div class="cookie-btns">
+                <button @click="showModal('cookie-modal')" class="btn">${cookiePageBtn5}</button>
+                <button @click="cookieAgree" class="btn">${cookiePageBtn4}</button>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            isHidden: false,
+        };
+    },
+    beforeMount() {
+        this.isHidden = localStorage.getItem('cookie_agree') ? true : false
+    },
+    methods: {
+        cookieAgree() {
+            localStorage.setItem('cookie_agree', 'true');
+            setCookies(1);
+            this.isHidden = true;
+        },
+    },
+});
+
+$(document).ready(function () {
+  $('#app').addClass('loaded');
+})
+
+$(window).on('load', function () {
+    // 禁用自動編碼
+    $.cookie.raw = true;
+    const essential = $.cookie('essential')
+    const config = essential ? JSON.parse(essential) : { analyticcookie: 0, functionalcookie: 0, marketingcookie: 0, rightBox: 0 }
+    if (!essential) $.cookie('essential', JSON.stringify(config))
+
+    if (config.functionalcookie === 0) {
+        $.removeCookie('cookie_agree', { path: '/' });
+    }
+    if (config.analyticcookie === 0) {
+        $.removeCookie('_ga', { path: '/' });
+        $.removeCookie('_ga_F99KXEB97J', { path: '/' });
+        $.removeCookie('_fbp', { path: '/' });
+        $.removeCookie('_ga', { path: '/', domain: '.icybox.de' });
+        $.removeCookie('_ga_F99KXEB97J', { path: '/', domain: '.icybox.de' });
+        $.removeCookie('_fbp', { path: '/', domain: '.icybox.de' });
+        $.removeCookie('_ga_5XK83QKW0W', { path: '/', domain: '.icybox.de' });
+        $.removeCookie('_gat_gtag_UA_187579170_1', { path: '/', domain: '.icybox.de' });
+        $.removeCookie('_gid', { path: '/', domain: '.icybox.de' });
+    }
+    if (config.marketingcookie === 0) { /* ... */}
+});
+
 const vm = new Vue({
     el: '#app',
     component: {},
@@ -59,7 +301,7 @@ const vm = new Vue({
 
         // this.getBanners();
         // this.getProductCategories();
-        this.getConfigurator();
+        // this.getConfigurator(); // NOTE disabled
         // this.getPhotoWalls();
         // this.getNews();
 
@@ -80,37 +322,6 @@ const vm = new Vue({
         //     });
         // }
 
-
-        const $owlCarousel = $('.owl-carousel');
-        if ($owlCarousel.length) {
-            if (this.isMobile()) {
-                $(document).ready(function () {
-                    $owlCarousel.owlCarousel({
-                        items: 1,
-                        loop: true,
-                        nav: true,
-                        autoplay: true,
-                        dots: false,
-                        autoplayTimeout: 8000,
-                        autoplayHoverPause: true,
-                        video: true
-                    });
-                });
-            } else {
-                $(document).ready(function () {
-                    $owlCarousel.owlCarousel({
-                        items: 1,
-                        loop: true,
-                        nav: true,
-                        autoplay: true,
-                        // dots: false,
-                        autoplayTimeout: 8000,
-                        autoplayHoverPause: true,
-                        video: true
-                    });
-                });
-            }
-        }
 
         // const $albumCarousel = $('.album-carousel');
         // if ( $albumCarousel.length ) {
@@ -320,13 +531,5 @@ const vm = new Vue({
                 fx($(this), $(this).scrollTop());
             });
         },
-        isMobile() {
-            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                return true
-            } else {
-                return false
-            }
-        }
-
     }
 });
